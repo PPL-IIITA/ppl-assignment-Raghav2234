cd3f0eed791d798682f571f0e281ce8e073d5b4a
Raghav2234 <raghavtayal2@gmail.com>
2017-04-09 11:34:32 +0530

    Add files via upload

---
+++ Boy.java
@@ -0,0 +1,14 @@
+/**
+ * Characteristics of boys
+ * @author Raghav Tayal
+ *
+ */
+public class Boy {
+	String name;
+	int Intelligence;
+	int Attractiveness;
+	int budget;
+	int att_req;
+	int status;
+	String type;
+}
